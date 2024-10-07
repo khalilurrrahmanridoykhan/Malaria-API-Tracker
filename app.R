3788c0283ba4cdad55beb677f466e53cce3617b8
khalilurrrahmanridoykhan <khalilurrahmanridoykhan@gmail.com>
2024-10-08 03:57:58 +0600

    covid map

--- app.R
+++ app.R
@@ -14,15 +14,21 @@ list.files("MaariaApiTracker/R") %>%
   )
 
 df <- read_csv(here::here("./data/COVID_data_2023-03-09.csv"))
+df$date <- as.Date(df$date, format = "%Y-%m-%d")
 
-# print(df$date)
+coords <- read_csv(here::here("./data/world_country_and_usa_states_latitude_and_longitude_values.csv"))
+
+coords_df <- df  %>%
+  inner_join(coords, by = c("country" = "country"))
+
+# print(coords_df)
 countrys_groupings(df)
 
 
 ui <- dashboardPage(
   dashboardHeader(
     titleWidth = "300px",
-    title = "Malaria API Tracker, Bangladesh",
+    title = "Covid Data Analysis, Worldwide",
     tags$li(
       class = "dropdown",
       tags$a(
@@ -52,8 +58,8 @@ ui <- dashboardPage(
         tags$li(tags$a(href = "#shiny-tab-yearlydeaths1", "Yearly Malaria Cases and Deaths", `data-toggle` = "tab")), # nolint
         tags$li(tags$a(href = "#shiny-tab-ageandgender1", "Age and Gender", `data-toggle` = "tab")), # nolint
         tags$li(tags$a(href = "#shiny-tab-species1", "Shecies Wish Malaria Cases (Pf/Pv/Mixed/Total)", `data-toggle` = "tab")), # nolint
-        tags$li(tags$a(href = "#shiny-tab-casevsservere1", "Case Vs Severe Malaria", `data-toggle` = "tab")), # nolint
-        tags$li(tags$a(href = "#shiny-tab-tast1", "Tast, Case and Test Positivily Rate", `data-toggle` = "tab")), # nolint
+        tags$li(tags$a(href = "#shiny-tab-casevsservere1", "Countrys Category Plot", `data-toggle` = "tab")), # nolint
+        tags$li(tags$a(href = "#shiny-tab-tast1", "Countrys Plot", `data-toggle` = "tab")), # nolint
       ),
     ),
     tags$li(
@@ -92,7 +98,17 @@ ui <- dashboardPage(
       ),
       tabItem(
         tabName = "time",
-        h1("Time Series API MAP")
+        div(
+          class = "row",
+          div(
+            class = "col-sm-2 col-md-2 col-lg-2",
+            selectInput("date", "Select Date:", choices = unique(coords_df$date), selected = "2023-02-16")
+          ),
+          div(
+            class = "col-sm-10 col-md-10 col-lg-10",
+leafletOutput("covidMap", width = "100%", height = "740px")
+          )
+        )
       ),
       tabItem(
         tabName = "bednet",
@@ -128,22 +144,25 @@ ui <- dashboardPage(
       ),
       tabItem(
         tabName = "casevsservere1",
-                div(
-                  class = "row",
-                  div(
-                    class = "col-sm-3 col-md-3 col-lg-3",
-                    selectInput("country1", "Select Country:", choices = NULL, selected = NULL),
-                    selectInput("metric", "Select Metric:",
-                                choices = c("Cumulative Cases" = "cumulative_cases",
-                                            "New Cases Past Week" = "new_cases_past_week",
-                                            "Cumulative Deaths" = "cumulative_deaths",
-                                            "New Deaths Past Week" = "new_deaths_past_week"))
-                  ),
-                  div(
-                    class = "col-sm-9 col-md-9 col-lg-9",
-                    plotOutput("pieChart")
-                  )
-                )
+        div(
+          class = "row",
+          div(
+            class = "col-sm-3 col-md-3 col-lg-3",
+            selectInput("country1", "Select Country:", choices = NULL, selected = NULL),
+            selectInput("metric", "Select Metric:",
+              choices = c(
+                "Cumulative Cases" = "cumulative_cases",
+                "New Cases Past Week" = "new_cases_past_week",
+                "Cumulative Deaths" = "cumulative_deaths",
+                "New Deaths Past Week" = "new_deaths_past_week"
+              )
+            )
+          ),
+          div(
+            class = "col-sm-9 col-md-9 col-lg-9",
+            plotOutput("pieChart")
+          )
+        )
       ),
       tabItem(
         tabName = "tast1",
@@ -197,7 +216,6 @@ server <- function(input, output, session) {
       addTiles() %>%
       addMarkers(lng = -74.0060, lat = 40.7128, popup = "Map 2 - New York")
   })
-  df$date <- as.Date(df$date, format = "%Y-%m-%d")
 
   observe({
     updateSelectInput(session, "country", choices = unique(df$country))
@@ -212,15 +230,19 @@ server <- function(input, output, session) {
         date <= input$dateRange[2]
       )
   })
-  output$cumulativeCasesPlot <- renderPlot({
-    ggplot(filtered_data(), aes(x = date, y = cumulative_cases)) +
-      geom_line(color = "blue", size = 1) +
-      labs(
-        title = "Cumulative COVID-19 Cases Over Time",
-        x = "date", y = "cumulative_cases"
-      ) +
-      theme_minimal()
-  }, height = 700, width = 1000)
+  output$cumulativeCasesPlot <- renderPlot(
+    {
+      ggplot(filtered_data(), aes(x = date, y = cumulative_cases)) +
+        geom_line(color = "blue", size = 1) +
+        labs(
+          title = "Cumulative COVID-19 Cases Over Time",
+          x = "date", y = "cumulative_cases"
+        ) +
+        theme_minimal()
+    },
+    height = 700,
+    width = 1000
+  )
 
   # Pai Chart
 
@@ -231,36 +253,68 @@ server <- function(input, output, session) {
     req(input$country1)
     df %>% filter(country == input$country1)
   })
-  output$pieChart <- renderPlot({
-    req(filtered_data1())
+  output$pieChart <- renderPlot(
+    {
+      req(filtered_data1())
 
-    data <- filtered_data1()
-    metric <- input$metric
+      data <- filtered_data1()
+      metric <- input$metric
 
-    # Create a summary for the pie chart
-    summary_data <- data %>%
-      summarise(
-        Value = sum(!!sym(metric), na.rm = TRUE)
-      ) %>%
-      mutate(Remaining = sum(df[[metric]], na.rm = TRUE) - Value) %>%
-      pivot_longer(cols = c(Value, Remaining), names_to = "Category", values_to = "Count") %>%
-      mutate(Percentage = Count / sum(Count) * 100)
+      # Create a summary for the pie chart
+      summary_data <- data %>%
+        summarise(
+          Value = sum(!!sym(metric), na.rm = TRUE)
+        ) %>%
+        mutate(Remaining = sum(df[[metric]], na.rm = TRUE) - Value) %>%
+        pivot_longer(cols = c(Value, Remaining), names_to = "Category", values_to = "Count") %>%
+        mutate(Percentage = Count / sum(Count) * 100)
 
-    # Create the pie chart
-    ggplot(summary_data, aes(x = "", y = Count, fill = Category)) +
-      geom_bar(stat = "identity", width = 1) +
-      coord_polar(theta = "y") +
-      labs(title = paste(input$country1, metric), x = NULL, y = NULL) +
-      theme_void() +
-      theme(legend.position = "right") +
-      scale_fill_manual(values = c("Value" = "#ff4000", "Remaining" = "#d9ff00")) +
-      geom_text(aes(label = paste0(round(Percentage, 1), "%")), position = position_stack(vjust = 0.5)) +
-      theme(plot.margin = margin(10, 10, 10, 10, "pt")) +
-      theme(plot.title = element_text(size = 20), legend.text = element_text(size = 12))
-  }, height = 700, width = 1000)
-
-  # Declare global variables to avoid binding issues
+      # Create the pie chart
+      ggplot(summary_data, aes(x = "", y = Count, fill = Category)) +
+        geom_bar(stat = "identity", width = 1) +
+        coord_polar(theta = "y") +
+        labs(title = paste(input$country1, metric), x = NULL, y = NULL) +
+        theme_void() +
+        theme(legend.position = "right") +
+        scale_fill_manual(values = c("Value" = "#ff4000", "Remaining" = "#d9ff00")) +
+        geom_text(aes(label = paste0(round(Percentage, 1), "%")), position = position_stack(vjust = 0.5)) +
+        theme(plot.margin = margin(10, 10, 10, 10, "pt")) +
+        theme(plot.title = element_text(size = 20), legend.text = element_text(size = 12))
+    },
+    height = 700,
+    width = 1000
+  )
   globalVariables(c("country", "Value", "Count", "Category"))
+
+  #Covid Map
+
+    filtered_data2 <- reactive({
+    req(input$date)
+    coords_df %>% filter(date == input$date)
+  })
+
+    output$covidMap <- renderLeaflet({
+    leaflet(filtered_data2()) %>%
+      addTiles() %>%
+      addCircleMarkers(
+      ~longitude, ~latitude,
+      radius = ~sqrt(cumulative_cases) / 500, # Adjust the size for better visibility
+      color = "red",
+      stroke = FALSE, fillOpacity = 0.7,
+      popup = ~paste0(
+      "<strong>Country: </strong>", country, "<br>",
+      "<strong>Date: </strong>", date, "<br>",
+      "<strong>Cumulative Cases: </strong>", cumulative_cases, "<br>",
+      "<strong>New Cases Past Week: </strong>", new_cases_past_week, "<br>",
+      "<strong>Cumulative Deaths: </strong>", cumulative_deaths, "<br>",
+      "<strong>New Deaths Past Week: </strong>", new_deaths_past_week
+      )
+      ) %>%
+      setView(lng = mean(filtered_data2()$longitude), lat = mean(filtered_data2()$latitude), zoom = 2) # Adjust the zoom level for better visibility
+    })
+
+
+
 }
 
 shinyApp(ui = ui, server = server)
